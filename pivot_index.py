78b1048d52d22555b2c87c77f42d20bc36040413
prashantkunivision <pckundagol@gmail.com>
2026-03-31 17:21:02 +0530

    Update pivot_index.py

--- pivot_index.py
+++ pivot_index.py
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int pivotIndex(int[] nums) {
+       int n = nums.length;
+    for(int i=0; i<n; i++){
+        int left_sum=0;
+        for(int j=0; j<i; j++){
+            left_sum+=nums[j];
+        }
+        int right_sum=0;
+        for(int k=i+1; k<n; k++){
+            right_sum+=nums[k];
+        }
+        if (left_sum==right_sum){
+            return i;
+        }
+    }
+    return -1; 
